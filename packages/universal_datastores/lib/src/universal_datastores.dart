dda9658184c689f072e44ef279bdbddbcb1057c6
vit con lon ton <hoanghiep97@hotmail.com>
2024-10-08 02:11:12 +0700

    init universal_datastores

---
+++ packages/universal_datastores/lib/src/universal_datastores.dart
@@ -0,0 +1,7 @@
+/// {@template universal_datastores}
+/// Dart package which manages data stores are commonly used.
+/// {@endtemplate}
+class UniversalDatastores {
+  /// {@macro universal_datastores}
+  const UniversalDatastores();
+}
---
+++ packages/universal_datastores/lib/universal_datastores.dart
@@ -0,0 +1,4 @@
+/// Dart package which manages data stores are commonly used.
+library;
+
+export 'src/universal_datastores.dart';
